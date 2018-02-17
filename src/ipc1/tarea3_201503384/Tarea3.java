ac8818fba1325373390e4a6cf3ba3fca043789d9
diego <diego@192.168.0.21>
2018-02-17 17:56:55 -0600

    Tarea3 inicializada

--- src/ipc1/tarea3_201503384/Tarea3.java
+++ src/ipc1/tarea3_201503384/Tarea3.java
@@ -15,7 +15,8 @@ public class Tarea3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Menu menu = new Menu();
+        menu.principal();
     }
     
 }
