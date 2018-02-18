8a0c7bdea5ea7c9cd581561a18a92de3cd315994
diego <diego@192.168.0.21>
2018-02-17 20:35:55 -0600

    Tarea3 en proceso

---
+++ src/ipc1/tarea3_201503384/ContadorDigitos.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ipc1.tarea3_201503384;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author diego
+ */
+public class ContadorDigitos {
+
+    Scanner teclado = new Scanner(System.in);
+    private int opcion, numero;
+
+    public void menu() {
+        do {
+            System.out.println("\n1. Ingresar numero \n2. Mostrar numero de digitos \n3. Menu principal");
+            opcion = teclado.nextInt();
+            
+            switch(opcion){
+                case 1:
+                    ingresarNumero();
+                    break;
+                case 2:
+                    mostrarDigitos(numero);
+                    break;
+                case 3:
+                    System.out.println("");
+                    Menu menuPrincipal = new Menu();
+                    menuPrincipal.principal();
+                    break;
+            }
+        } while (opcion < 1 || opcion > 3);
+    }
+    
+    public void ingresarNumero(){
+        System.out.println("\nIngrese un numero");
+        numero = teclado.nextInt();
+        menu();
+    }
+    
+    public void mostrarDigitos(int numero){
+        int contador = 0;
+        while(numero > 0){
+            numero = numero / 10;
+            contador++;
+        }
+        System.out.println("\nEl numero " + this.numero + " tiene " + contador + " digitos.");
+        menu();
+    }
+}
