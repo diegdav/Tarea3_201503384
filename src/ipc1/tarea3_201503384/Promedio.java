b1a7ca993d8ab215c4faedf692242862bb06100f
diego <diego@192.168.0.21>
2018-02-24 18:23:39 -0600

    Practica1 en proceso

--- src/ipc1/tarea3_201503384/Promedio.java
+++ src/ipc1/tarea3_201503384/Promedio.java
@@ -16,11 +16,11 @@ public class Promedio {
 
     Scanner teclado = new Scanner(System.in);
     private int opcion;
-    private final String[][] titulos = {{"ID", "Nota 1", "Nota 2", "Nota 3", "Nota 4", "Promedio"}};
-    private final int[][] notas;
+    private final String[][] titulos = {{"ID", "Nota 1", "Nota 2", "Nota 3", "Nota 4", "Promedio"}};    //encabezado de cada columna
+    private final int[][] notas;                                                                        //matriz para que el usuario ingrese las notas
 
     public Promedio() {
-        notas = new int[6][6];
+        notas = new int[6][6];                                                  //se define el tamaño de la matriz
     }
 
     public void menu() {
@@ -34,11 +34,15 @@ public class Promedio {
                         ingresar();
                         break;
                     case 2:
-                        mostrar();
+                        if (notas[0][0] == 0) {
+                            System.out.println("\nDebe de ingresar de primero las notas");
+                            menu();
+                        } else {
+                            mostrar();
+                        }
                         break;
                     case 3:
                         System.out.println("");
-                        
                         Menu menuPrincipal = new Menu();
                         menuPrincipal.principal();
                         break;
@@ -54,11 +58,12 @@ public class Promedio {
 
     public void ingresar() {
         int i, j, nota, contador;
-        contador = 1;
+        contador = 1;                                               //es para mostrar el numero de nota que se vaya a ingresar
+        
         for (i = 0; i < 6; i++) {
             System.out.print("\nIngrese el id del estudiante: ");
-            notas[i][0] = teclado.nextInt();
-            for (j = 1; j < 5; j++) {
+            notas[i][0] = teclado.nextInt();                        //guarda el id en la matriz en la columna 0 y en cada fila correspondiente
+            for (j = 1; j < 5; j++) {                               //inicia desde 1 ya que el espacio 0 corresponde al id
                 do {
                     System.out.print("Ingrese la nota " + contador + " del estudiante " + notas[i][0] + ": ");
                     nota = teclado.nextInt();
@@ -66,10 +71,10 @@ public class Promedio {
                         System.out.println("\nDebe de ingresar una nota menor a 100 y mayor a 0");
                     }
                 } while (nota < 0 || nota > 100);
-                notas[i][j] = nota;
-                contador++;
-                if (contador == 5) {
-                    contador = 1;
+                notas[i][j] = nota;                                 //la nota ingresada se guardara en la fila y columna correspondiente
+                contador++;                                         //el contador lleva el control de cada nota ingresada
+                if (contador == 5) {                                //al momento de ingresar la nota 4 aumentara en 5 
+                    contador = 1;                                   //por lo tanto tiene que volver a iniciar desde 1
                 }
             }
         }
@@ -81,14 +86,14 @@ public class Promedio {
         int fila, columna;
         for (fila = 0; fila < 1; fila++) {
             for (columna = 0; columna < 6; columna++) {
-                System.out.printf(titulos[fila][columna] + "\t");
-            }
+                System.out.printf(titulos[fila][columna] + "\t");               //\t es para que haga tabulacion al momento de ingresar cada columna
+            }                                                                   //al recorrer el for, se muestra el contenido de la matriz titulos.
             System.out.println("");
         }
-        for (fila = 0; fila < 6; fila++) {
+        for (fila = 0; fila < 6; fila++) {                                      
             for (columna = 0; columna < 6; columna++) {
-                System.out.print(notas[fila][columna] + "\t");
-            }
+                System.out.print(notas[fila][columna] + "\t");                  //recorre la matriz notas y las muestra, dejando tabulacion por cada 
+            }                                                                   //columna
             System.out.println("");
         }
         menu();
@@ -97,10 +102,10 @@ public class Promedio {
     public void calcularPromedio() {
         int fila, columna;
         for (fila = 0; fila < 6; fila++) {
-            for (columna = 1; columna < 5; columna++) {
-                notas[fila][5] += notas[fila][columna];
+            for (columna = 1; columna < 5; columna++) {                         //recorre de 2 a 4 ya que en la posicion 1 se encuentra el id y en el 5 el promedio
+                notas[fila][5] += notas[fila][columna];                         //guarda en la ultima columna de cada fila la suma de cada nota recorrida
             }
-            notas[fila][5] = notas[fila][5] / 4;
+            notas[fila][5] = notas[fila][5] / 4;                                //al terminar de sumar cada nota la divide dentro de 4 para obtener el promedio
         }
     }
 }
